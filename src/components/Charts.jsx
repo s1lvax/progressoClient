f7a9a7f4171c037d28f90d4bc2471bee89e0cd0a
silva <silva.cesario49@gmail.com>
2023-05-01 15:46:32 +0200

    Added Basic Charts

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,31 +1,147 @@
 import axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { UserId } from "../contexts/UserId";
+import Chart from "chart.js/auto";
 
 function Charts() {
   const { userId } = useContext(UserId);
   const [weightStats, setWeightStats] = useState([]);
   const [bfStats, setBFStats] = useState([]);
+  const [dates, setDates] = useState([]);
   const [avgWeight, setAvgWeight] = useState(null);
   const [avgBf, setAvgBf] = useState(null);
+  const weightChartRef = useRef(null);
+  const bfChartRef = useRef(null);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3001/getStats/${userId}`)
       .then((response) => {
-        const data = JSON.parse(response.data); // Parse the JSON string to a JavaScript object
+        const data = JSON.parse(response.data);
         const weightData = data.weight;
         const bfData = data.bodyfat;
         const avgWeight = data.avgWeight;
         const avgBf = data.avgBf;
+        const dates = data.dates;
 
-        //all the stats into arrays
         setWeightStats(weightData);
         setBFStats(bfData);
-
-        //average values
         setAvgWeight(avgWeight);
         setAvgBf(avgBf);
+
+        if (weightChartRef.current) {
+          new Chart(weightChartRef.current, {
+            type: "line",
+            data: {
+              labels: dates,
+              datasets: [
+                {
+                  label: "Weight",
+                  data: weightData,
+                  borderColor: "#7a00b4",
+                  backgroundColor: "rgba(122, 0, 180, 0.1)",
+                  borderWidth: 3,
+                  fill: true,
+                  lineTension: 0.3,
+                },
+              ],
+            },
+            options: {
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      beginAtZero: true,
+                      fontColor: "#b1b1b1",
+                    },
+                    gridLines: {
+                      color: "rgba(177, 177, 177, 0.1)",
+                    },
+                  },
+                ],
+                xAxes: [
+                  {
+                    ticks: {
+                      fontColor: "#b1b1b1",
+                    },
+                    gridLines: {
+                      color: "rgba(177, 177, 177, 0.1)",
+                    },
+                  },
+                ],
+              },
+            },
+          });
+        }
+
+        if (bfChartRef.current) {
+          new Chart(bfChartRef.current, {
+            type: "line",
+            data: {
+              labels: dates,
+              datasets: [
+                {
+                  label: "Bodyfat",
+                  data: bfData,
+                  borderColor: "#b4009e",
+                  backgroundColor: "rgba(180, 0, 158, 0.1)",
+                  borderWidth: 3,
+                  fill: true,
+                  pointBackgroundColor: "#b4009e",
+                  pointBorderColor: "transparent",
+                  pointRadius: 6,
+                  pointHoverRadius: 8,
+                  shadowOffsetX: 0,
+                  shadowOffsetY: 5,
+                  shadowBlur: 10,
+                  shadowColor: "rgba(0, 0, 0, 0.5)",
+                  lineTension: 0.5,
+                },
+              ],
+            },
+            options: {
+              tooltips: {
+                mode: "index",
+                intersect: false,
+                callbacks: {
+                  label: function (tooltipItem, data) {
+                    var label =
+                      data.datasets[tooltipItem.datasetIndex].label || "";
+                    if (label) {
+                      label += ": ";
+                    }
+                    label += tooltipItem.yLabel + "%";
+                    return label;
+                  },
+                },
+              },
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      beginAtZero: true,
+                      max: 100,
+                      fontColor: "#b1b1b1",
+                    },
+                    gridLines: {
+                      color: "rgba(177, 177, 177, 0.1)",
+                    },
+                  },
+                ],
+                xAxes: [
+                  {
+                    ticks: {
+                      fontColor: "#b1b1b1",
+                    },
+                    gridLines: {
+                      color: "rgba(177, 177, 177, 0.1)",
+                    },
+                  },
+                ],
+              },
+            },
+          });
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -33,12 +149,13 @@ function Charts() {
   }, [userId]);
 
   return (
-    <div>
-      <div>Your weight stats are : {weightStats}</div>
-      <div>Your BF stats are: {bfStats}</div>
-      <br />
-      <div>Your Average Weight is: {avgWeight}</div>
-      <div>Your Average BF is: {avgBf}</div>
+    <div style={{ display: "flex" }}>
+      <div style={{ flexBasis: "50%", flexGrow: 1 }}>
+        <canvas ref={weightChartRef} />
+      </div>
+      <div style={{ flexBasis: "50%", flexGrow: 1 }}>
+        <canvas ref={bfChartRef} />
+      </div>
     </div>
   );
 }
